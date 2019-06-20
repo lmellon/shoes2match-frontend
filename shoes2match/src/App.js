208dda6be83e104697fba34ce1f67c4a66f0865a
Lauren Mellon <laumellon@gmail.com>
2019-06-20 18:54:15 -0400

    created a React App, installed dependencies

---
+++ shoes2match/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+      <Router>
+        <div className="App">
+            Hey there
+        </div>
+    </Router>
+  );
+}
+
+export default App;
