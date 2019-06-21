9af38e2091cfc2fd30bd3528b5267c1934c20aa9
Lauren Mellon <laumellon@gmail.com>
2019-06-21 14:06:00 -0400

    added form and methods to create component.  connected to db and successfully added shoes

--- shoes2match/src/App.js
+++ shoes2match/src/App.js
@@ -7,25 +7,27 @@ import Landing from './components/landing';
 import ShoesList from './components/shoes-list';
 
 
-function App() {
-  return (
-      <Router>
-        <div className="App">
-        <nav>
-            <ul>
-                <li><Link to="/">Shoes2match</Link></li>
-                <li><Link to="/list"> Shoe List </Link></li>
-                <li><Link to="/create"> Add Shoe</Link></li>
-            </ul>
-        </nav>
-        <br />
-            <Route path="/" exact component={Landing} />
-            <Route path="/list" component={ShoesList} />
-            <Route path="/edit/:id" component={EditShoe} />
-            <Route path="/create" component={CreateShoe} />
-        </div>
-    </Router>
-  );
+class App extends Component {
+    render(){
+        return (
+          <Router>
+            <div className="App">
+            <nav>
+                <ul>
+                    <li><Link to="/">Shoes2match</Link></li>
+                    <li><Link to="/list"> Shoe List </Link></li>
+                    <li><Link to="/create"> Add Shoe</Link></li>
+                </ul>
+            </nav>
+            <br />
+                <Route path="/" exact component={Landing} />
+                <Route path="/list" component={ShoesList} />
+                <Route path="/edit/:id" component={EditShoe} />
+                <Route path="/create" component={CreateShoe} />
+            </div>
+        </Router>
+      );
+    }
 }
 
 export default App;
--- shoes2match/src/components/create-shoe.js
+++ shoes2match/src/components/create-shoe.js
@@ -1,10 +1,113 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class CreateShoe extends Component {
+    constructor(props){
+    super(props);
+
+    this.state = {
+        style: '',
+        brand: '',
+        color: '',
+        image: '',
+        size: ''
+    }
+}
+    // method to handle input
+    handleChange = (e) => {
+        this.setState({ [e.target.id]: e.target.value })
+    }
+
+    // method to handle submit
+    handleSubmit = (e) => {
+        e.preventDefault();
+        // create variable to be the new shoes
+        let newShoe = {
+            style: this.state.style,
+            brand: this.state.brand,
+            color: this.state.color,
+            image: this.state.image,
+            size: this.state.size
+        };
+        console.log(newShoe);
+
+        // add newShoe to database using axios
+        axios.post('http://localhost:3000/shoes/add', newShoe)
+            .then(res => console.log(res.data))
+
+        // clear Form
+        this.clearForm();
+    }
+
+    // method to clear out form after submit
+    clearForm = () => {
+        this.setState({
+            style: '',
+            brand: '',
+            color: '',
+            image: '',
+            size: ''
+        })
+    }
+
+
     render() {
         return(
             <div>
-                Create Shoe Component
+                <h3>Add Shoes to your Collection</h3>
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-type">
+                        <label> Style: </label>
+                        <input type="text"
+                                className="form-item"
+                                value={this.state.style}
+                                onChange={this.handleChange}
+                                id="style"
+                        />
+                    </div>
+                    <div className="form-type">
+                        <label> Brand: </label>
+                        <input type="text"
+                                className="form-item"
+                                value={this.state.brand}
+                                onChange={this.handleChange}
+                                id="brand"
+                        />
+                    </div>
+                    <div className="form-type">
+                        <label> Color: </label>
+                        <input type="text"
+                                className="form-item"
+                                value={this.state.color}
+                                onChange={this.handleChange}
+                                id="color"
+                        />
+                    </div>
+                    <div className="form-type">
+                        <label> Size: </label>
+                        <input type="text"
+                                className="form-item"
+                                value={this.state.size}
+                                onChange={this.handleChange}
+                                id="size"
+                        />
+                    </div>
+                    <div className="form-type">
+                        <label> Image: </label>
+                        <input type="text"
+                                className="form-item"
+                                value={this.state.image}
+                                onChange={this.handleChange}
+                                id="image"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input type="submit"
+                                value="Add To Collection"
+                                className="form-submit"
+                            />
+                    </div>
+                </form>
             </div>
         )
     }
